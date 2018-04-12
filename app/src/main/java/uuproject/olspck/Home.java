9adbf45d15edec4b344de40b0a30c01ad7903189
mdickson2009 <mdickson2009@hotmail.co.uk>
2018-04-12 17:28:04 +0100

    0.6

--- app/src/main/java/uuproject/olspck/Home.java
+++ app/src/main/java/uuproject/olspck/Home.java
@@ -2,19 +2,15 @@ package uuproject.olspck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
 
 
-    private  ImageView mImageView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        mImageView = (ImageView) findViewById(R.id.imageView);
+        setContentView(R.layout.activity_home2);
+    }
 
     }
-}
+
